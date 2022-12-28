38e72c9ced1e051e5d4d88d2cd491705489b1b92
airshad <m.aamir_irshad@yahoo.com>
2022-12-28 22:25:48 +0000

    Adding Amazon Load Balancer

--- main.tf
+++ main.tf
@@ -42,6 +42,47 @@ resource "aws_instance" "blog" {
   }
 }
 
+
+// Load balancer Code for our instance
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnet
+  security_groups    = [module.blog_security_group.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
