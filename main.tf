fa2c701df156158f751ec0503ae5b30187baa756
airshad <m.aamir_irshad@yahoo.com>
2022-12-28 20:20:07 +0000

    Adding VPC , Security Group and Security Rules with Module approach

--- main.tf
+++ main.tf
@@ -14,31 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-// Defining VPC block
 data "aws_vpc" "default" {
   default = true
 }
 
-//Define instance resource for AWS
 resource "aws_instance" "blog" {
-  ami              = data.aws_ami.app_ami.id
-  instance_type    = var.instance_type
-  vpc_security_group_ids = [module.blog.security_group_id]
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Instance Created From TF"
+    Name = "Learning Terraform"
   }
 }
 
-// Define security group resource
-module "blog" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.16.2"
-  name    = "blog"
-  vpc_id  = data.aws_vpc.default.id
+  version = "4.13.0"
 
-  ingress_rules       = ["http_80_tcp","https_443_tcp"]
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules        = [ "all-all" ]
+  egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
