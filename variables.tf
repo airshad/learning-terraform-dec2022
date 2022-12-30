302694e80b6f34202c54e8cce12a92092540ed29
airshad <m.aamir_irshad@yahoo.com>
2022-12-30 11:08:19 +0000

    Adding Amazon Load Balancer with Auto Scaling enable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,54 @@ variable "instance_type" {
  description = "Type of EC2 instance to provision"
  default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name and Owner for AMI"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner  = "979382823631"
+  }
+}
+
+variable "environment" {
+  description = "Environment Variables for Development Env"
+
+  type = object({
+    name            = string
+    network_prefix  = string
+  })
+  default = {
+    name            = "dev"
+    network_prefix  = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "Minimum size of instance in ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Maximum size of instance in ASG"
+  default     = 2
+}
+
+variable "network" {
+  description = "Network Port and Protocol"
+
+  type = object({
+    port     = number
+    protocol = string
+  })
+
+  default = {
+    port     = 80
+    protocol = "HTTP"
+  }
+}
